ce6361b26e9d3ba7bc7addccaf0bd96827c0544a
Paulo Aragao <aragao.paulo@gmail.com>
2021-10-27 15:26:54 +0000

    fixed merge

--- pcluster-amplified/src/Components/InstanceCards.js
+++ pcluster-amplified/src/Components/InstanceCards.js
@@ -6,23 +6,6 @@ import AirplayIcon from '@mui/icons-material/Airplay';
 import { API, graphqlOperation } from 'aws-amplify'; 
 import { listInstances } from '../graphql/queries';
 import { deleteInstance } from '../graphql/mutations';
-<<<<<<< HEAD
-import { onCreateInstance, onDeleteInstance } from '../graphql/subscriptions';
-
-const initialSeverity = {
-    message: '',
-    severity: ''
-};
-
-export default function BasicCard() {
-
-    const [instances, setInstances] = useState([]);
-    const [snackSeverity, setSnackSeverity] = useState(initialSeverity);
-    const [snackOpen, setSnackOpen] = useState(false);
-
-    useEffect(() => {
-        fetchInstances()
-=======
 import { onCreateInstance, onDeleteInstance, onUpdateInstance } from '../graphql/subscriptions';
 
 export default function BasicCard() {
@@ -31,44 +14,13 @@ export default function BasicCard() {
     
     useEffect(() => {
         fetchInstances();
->>>>>>> refactor_lambda
         //const interval = setInterval(() => {
         //    fetchInstances()
         //  }, 5000);
         //return () => clearInterval(interval);
     }, [])
-<<<<<<< HEAD
-
-    const handleClose = (event, reason) => {
-        if (reason === 'clickaway') {
-            return;
-        }
-
-        setSnackOpen(false);
-        };
-
-        const action = (
-        <>
-            <IconButton 
-            size="small"
-            aria-label="close"
-            color="inherit"
-            onClick={handleClose}
-            >
-            <CloseIcon fontSize="small" />
-            </IconButton>
-        </>
-    );
-
-    function setSeverity(key, value) {
-        setSnackSeverity({ ...snackSeverity, [key]: value })
-    }
-
-    const subscription = API.graphql(graphqlOperation(onCreateInstance))
-=======
     
     const onCreateInstanceSubscription = API.graphql(graphqlOperation(onCreateInstance))
->>>>>>> refactor_lambda
     .subscribe({
         next: (instanceData) => {
             console.log(instanceData.value.data)
@@ -78,17 +30,6 @@ export default function BasicCard() {
         },
         error: err => console.warn(err)
     });
-<<<<<<< HEAD
-
-    const deleteSubs = API.graphql(graphqlOperation(onDeleteInstance))
-    .subscribe({
-        next: (instanceData) => {
-            console.log(instanceData.value.data)
-            const deletedInstance = instanceData.value.data.onDeleteInstance
-            //setInstances([...instances, deletedInstance])
-        }
-    })
-=======
     
     const onDeleteInstanceSubscription = API.graphql(graphqlOperation(onDeleteInstance))
     .subscribe({
@@ -111,7 +52,6 @@ export default function BasicCard() {
         },
         error: err => console.warn(err)
     });
->>>>>>> refactor_lambda
 
     async function fetchInstances() {
         try {
@@ -123,37 +63,6 @@ export default function BasicCard() {
         } catch (err) { console.warn('error fetching instances: ', err) }
     }
 
-<<<<<<< HEAD
-    function createLink(ip, pkid) {
-        if (ip != null) {
-            return(
-            <>
-                <Grid container>
-                    <Grid item xs={4} md={2} lg={4}>
-                        <Button 
-                            button
-                            component="a"
-                            href={'https://' + ip + ':8443'}
-                            target="_blank" 
-                            rel="noopener noreferrer"
-                            size="small"
-                        >
-                            <AirplayIcon />
-                        </Button>
-                    </Grid>
-                    <Grid item xs={4} md={2} lg={4}>
-                        <Button
-                            button
-                            size="small"
-                            onClick={() => delInstance(pkid)}
-                            defaultValue={pkid}
-                            id={pkid}
-                        >
-                            <DeleteIcon />
-                        </Button>
-                    </Grid>
-                </Grid>
-=======
     function createLink(e) {
         console.log(e)
         if (e.publicip != null) {
@@ -177,38 +86,17 @@ export default function BasicCard() {
                 >
                     Delete instance
                 </Button>
->>>>>>> refactor_lambda
             </>
             )
         } 
     }
 
     async function delInstance(id) {
-<<<<<<< HEAD
-        console.log(id)
-        if (id != null) {
-            try {
-                await API.graphql(graphqlOperation(deleteInstance, {input: {id} }))
-                console.log('success deleting id: ', id)
-                setSeverity('severity', 'success')
-                setSeverity('message', 'instance deletion submitted')
-                setSnackOpen(true)
-                fetchInstances();
-            } catch (err) { 
-                console.warn('error deleting id: ', err)
-                setSeverity('severity', 'error')
-                setSeverity('severity', 'error deleting instance')
-                setSnackOpen(true)
-            }
-        } else {
-            console.warn('pkId is undefined')
-=======
         try {
             const deleteResponse = await API.graphql(graphqlOperation(deleteInstance, {input: { id } }))
             console.log('instance deleted from DynamoDB successfully');
           } catch (err) { 
             console.warn('error deleting instance: ', err)
->>>>>>> refactor_lambda
         }
     }
 
@@ -233,11 +121,7 @@ export default function BasicCard() {
                             <Typography variant="body2">
                                 ID: {instance.instanceId}
                             </Typography>
-<<<<<<< HEAD
-                            {createLink(instance.publicip, instance.id)} 
-=======
                             {createLink(instance)} 
->>>>>>> refactor_lambda
                         </Paper>
                         <Snackbar 
                             open={snackOpen}
